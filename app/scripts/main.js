d8803d6205004b053838072637ba0161ecc20350
Francesco Pontillo <francescopontillo@gmail.com>
2015-03-27 01:01:47 +0100

    Discard video.js, use native HTML5 video and dash.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,10 +3,5 @@
 /* jshint devel:true */
 
 (function(){
-    var url = "http://54.201.151.65/livesim/tfdt_32/testpic_2s/Manifest.mpd";
-    var context = new Dash.di.DashContext();
-    var player = new MediaPlayer(context);
-    player.startup();
-    player.attachView(document.querySelector("#videoPlayer"));
-    player.attachSource(url);
+
 })();
